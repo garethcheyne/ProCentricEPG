2419e214111065d885817292a9e99a75aae3515b
Gareth Cheyne <Gareth.Cheyne@nz.harveynorman.com>
2025-03-23 12:59:58 +1300

    main update

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-# ProCentricEPG
+# LG ProCentric EPG and Channel Bundle Creator
+
+## Overview
+
+This project is designed to model and format TV EPG (Electronic Program Guide) data for LG ProCentric servers. It fetches raw EPG data, processes it into a structured format, and ensures compatibility with the LG ProCentric server.
+
+## Features
+
+- Fetches and parses EPG data.
+- Maps data to structured models (`Event`, `Channel`, `ProgramGuide`).
+- Handles time zones, dates, and program durations.
+- Safe extraction of data with default values for missing fields.
+
+## Dependencies
+
+- `requests`: For fetching data.
+- `xml.etree.ElementTree`: For XML parsing.
+- `pydantic`: For data modeling.
+- `pytz`: For time zone handling.
+
+
+## LG ProCentric Server
+Preparing the data form importation is only the first step, you must host a Zip file on and accessable FTP server and has the file named correctly.
+
+ZIP Naming Convention = Procentric_EPG_{ISO Country Code ie NZL}_{Date YYYYMMDD}.zip
+
+JSON Naming Convention = Procentric_EPG.json
+
+JSON payload needs to be enclosed into the the ZIP file and places in the approperate directory for collections over FTP.
+
+### FTP Service
+Your file needs to sit in a sub directory from a logged in FTP user.
+
+Linux Users prospective
+/home/procentric/EPG/NZL
+
+FTP Users prospective
+/EPG/NZL
+
+
+### Expected JSON Format
+```
+{
+    "filetype": "Pro:Centric JSON Program Guide Data NZL",
+    "version": "0.1",
+    "fetchTime": "2022-06-24T13:22:44+1200",
+    "maxMinutes": 60,
+    "channels": [
+        {
+            "channelID": "1",
+            "name": "TVNZ One",
+            "resolution": "HD",
+            "events": [
+                {
+                    "eventID": "334242",
+                    "title": "6 News",
+                    "eventDescription": TVNZ New Zealand News",
+                    "rating": "TV-MA",
+                    "date": "2022-06-25",
+                    "startTime": "1800",
+                    "length": "60",
+                    "genre": "News"
+                }
+            ]
+        }
+    ]
+}
+```
+
+
+### EPG ID Mapping for PCD or Other.
+[EPG ID Mapping](./data/raw_channels.csv)
+
+## Channel Icon Bundle
+
+Will build and bundle the NZ Channels icon bundle for importing into PCD.
--- src/main.py
+++ src/main.py
@@ -4,30 +4,30 @@ from epg_sources.xmltv_net.main import XMLTV
 from epg_sources.sky_nz.main import SkyNZ_EPG
 
 
-# # Create XMLTV objects for different Australian cities with a clear description
-# source_sydney = XMLTV("https://xmltv.net/xml_files/Sydney.xml", "Pro:Centric JSON Program Guide Data AUS Sydney")
-# source_brisbane = XMLTV("https://xmltv.net/xml_files/Brisbane.xml", "Pro:Centric JSON Program Guide Data AUS Brisbane")
-# source_adelaide = XMLTV("https://xmltv.net/xml_files/Adelaide.xml", "Pro:Centric JSON Program Guide Data AUS Adelaide")
-# source_goldcoast = XMLTV("https://xmltv.net/xml_files/Goldcoast.xml", "Pro:Centric JSON Program Guide Data AUS Gold Coast")
+# Create XMLTV objects for different Australian cities with a clear description
+source_sydney = XMLTV("https://xmltv.net/xml_files/Sydney.xml", "Pro:Centric JSON Program Guide Data AUS Sydney")
+source_brisbane = XMLTV("https://xmltv.net/xml_files/Brisbane.xml", "Pro:Centric JSON Program Guide Data AUS Brisbane")
+source_adelaide = XMLTV("https://xmltv.net/xml_files/Adelaide.xml", "Pro:Centric JSON Program Guide Data AUS Adelaide")
+source_goldcoast = XMLTV("https://xmltv.net/xml_files/Goldcoast.xml", "Pro:Centric JSON Program Guide Data AUS Gold Coast")
 
-# # Function to fetch and save EPG data for a given source
-# def XMLTVProcess(source: XMLTV, location_tags: list, file_prefix: str):
-#     try:
-#         # Fetch and parse the XML data into the ProgramGuide model
+# Function to fetch and save EPG data for a given source
+def XMLTVProcess(source: XMLTV, location_tags: list, file_prefix: str):
+    try:
+        # Fetch and parse the XML data into the ProgramGuide model
         
-#         print(f"Fetching and parsing the XML data for {source.title}...")
+        print(f"Fetching and parsing the XML data for {source.title}...")
         
-#         program_guide = source.get_program_guide()
-#         print(f"Successfully fetched and parsed the XML data for {source.title}:")
-#         save_and_zip(program_guide, location_tags, file_prefix)
-#     except Exception as e:
-#         print(f"Error occurred while processing {source.title}: {e}")
+        program_guide = source.get_program_guide()
+        print(f"Successfully fetched and parsed the XML data for {source.title}:")
+        save_and_zip(program_guide, location_tags, file_prefix)
+    except Exception as e:
+        print(f"Error occurred while processing {source.title}: {e}")
 
-# # Fetch and save EPG data for Sydney, Brisbane, Adelaide, and Gold Coast
-# XMLTVProcess(source_sydney, ["EPG", "AU", "Sydney"], "Procentric_EPG_SYD")
-# XMLTVProcess(source_brisbane, ["EPG", "AU", "Brisbane"], "Procentric_EPG_BRN")
-# XMLTVProcess(source_adelaide, ["EPG", "AU", "Adelaide"], "Procentric_EPG_ADL")
-# XMLTVProcess(source_goldcoast, ["EPG", "AU", "GoldCoast"], "Procentric_EPG_GLD")
+# Fetch and save EPG data for Sydney, Brisbane, Adelaide, and Gold Coast
+XMLTVProcess(source_sydney, ["EPG", "AU", "Sydney"], "Procentric_EPG_SYD")
+XMLTVProcess(source_brisbane, ["EPG", "AU", "Brisbane"], "Procentric_EPG_BRN")
+XMLTVProcess(source_adelaide, ["EPG", "AU", "Adelaide"], "Procentric_EPG_ADL")
+XMLTVProcess(source_goldcoast, ["EPG", "AU", "GoldCoast"], "Procentric_EPG_GLD")
 
 
 
